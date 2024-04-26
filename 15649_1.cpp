94cb5828e239ff22691444fccfa4f4fb15ee5546
u-siop <jinwook.jun5@gmail.com>
2024-04-26 23:53:05 +0900

    until 2024.04.26

--- 15649_1.cpp
+++ 15649_1.cpp
@@ -3,28 +3,34 @@
 
 using namespace std;
 
-int n,m;
+int n, m;
 int arr[102];
 bool isused[102];
 
-void func(int k){
-  if(k==m){
-    for(int i=0; i<m; i++){
-      cout << arr[i] <<' ';
+void func(int k)
+{
+  if (k == m)
+  {
+    for (int i = 0; i < m; i++)
+    {
+      cout << arr[i] << ' ';
     }
     cout << '\n';
   }
-  for(int i=1; i<=n; i++){
-    if(!isused[i]){
+  for (int i = 1; i <= n; i++)
+  {
+    if (!isused[i])
+    {
       arr[k] = i;
-      isused[k] = true; 
-      func(k+1);
+      isused[k] = true;
+      func(k + 1);
       isused[k] = false;
     }
   }
 }
 
-int main(){
+int main()
+{
   ios::sync_with_stdio(0);
   cin.tie(0);
 
